87ba3fc43bc961fcfa7484359884b0127a677beb
Ocha-Misaki <himawari.158fuji@gmail.com>
2025-05-02 11:56:00 +0900

    adminのnamespaceを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :admins, controllers: {
     sessions: "admins/sessions"
   }
+
+  namespace :admins do
+    root "groceries#index"
+    resources :foods, only: [ :show, :new, :create, :edit, :update, :destroy ]
+    resources :food_sets
+    resources :food_set_items, only: [ :new, :create, :edit, :update, :destroy ]
+  end
 end
